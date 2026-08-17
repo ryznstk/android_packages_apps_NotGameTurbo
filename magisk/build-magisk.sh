490d740218f1fc799b1ee6e7cf994c03b960af00
AdarshGrewal <adarshgrewal@gmail.com>
2026-08-17 20:17:20 +0530

    NotGameTurbo: Update magisk module script
    Change-Id: I9cf9daeb3de74b3151386c095379fdd0d590f1ec

--- magisk/build-magisk.sh
+++ magisk/build-magisk.sh
@@ -23,8 +23,8 @@ cp "$REPO_DIR/privapp-permissions-notgameturbo.xml" "$BUILD/system/etc/permissio
 cat > "$BUILD/module.prop" <<EOF
 id=notgameturbo
 name=NotGameTurbo
-version=v1.0
-versionCode=1
+version=v1.3
+versionCode=103
 author=grewal
 description=Per-app touch game mode, super report rate, tuning and panel orientation for Xiaomi devices with the touchfeature HAL.
 EOF
@@ -38,6 +38,9 @@ allow priv_app hal_touchfeature_xiaomi_service service_manager find
 allow priv_app hal_touchfeature_xiaomi_default binder { call transfer }
 allow hal_touchfeature_xiaomi_default priv_app binder { call transfer }
 allow hal_touchfeature_xiaomi_default priv_app fd use
+allow platform_app vendor_sysfs_touch file { ioctl read write getattr lock append map open watch watch_reads }
+allow priv_app vendor_sysfs_touch file { ioctl read write getattr lock append map open watch watch_reads }
+typeattribute vendor_sysfs_touch mlstrustedobject
 EOF
 
 rm -f "$OUT_ZIP"
