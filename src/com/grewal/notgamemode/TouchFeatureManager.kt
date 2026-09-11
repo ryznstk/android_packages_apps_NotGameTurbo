4557d22ced8968d855cca6e0549cf28b5be2b31f
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-09-11 18:01:00 +0800

    Add isGameModeActive method to TouchFeatureManager

--- src/com/grewal/notgamemode/TouchFeatureManager.kt
+++ src/com/grewal/notgamemode/TouchFeatureManager.kt
@@ -100,6 +100,9 @@ object TouchFeatureManager {
         selectFallbackIfNeeded()
         current.setModeValue(mode, value)
     }
+
+    fun isGameModeActive(): Boolean = isAvailable() && (queryMode(TOUCH_GAME_MODE).cur == 1)
+    
     fun setGameMode(enabled: Boolean) {
         Log.i(TAG, "setGameMode: $enabled")
         setModeValue(TOUCH_GAME_MODE, if (enabled) 1 else 0)
